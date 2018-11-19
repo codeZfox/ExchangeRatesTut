b49eb40f812dcf0a56dfe78ed9c0715245681ac2
Eugene <codezfox@live.ru>
2018-11-20 00:47:05 +0300

    Added Crashlytics

--- app/src/main/java/com/codezfox/exchangeratesmvp/App.kt
+++ app/src/main/java/com/codezfox/exchangeratesmvp/App.kt
@@ -2,12 +2,15 @@ package com.codezfox.exchangeratesmvp
 
 import android.app.Application
 import com.codezfox.exchangeratesmvp.di.DaggerUtils
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
+        Fabric.with(this, Crashlytics())
         DaggerUtils.buildComponents(this)
     }
 }
